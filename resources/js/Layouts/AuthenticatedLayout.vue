29f0381910ae240cec53befa84b6fc02d371e829
Nic Howe <nichowe.garcia@gmail.com>
2023-09-08 15:09:53 -0400

    set up TS

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,13 +1,4 @@
 <script setup>
-// import { ref } from 'vue';
-// import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-// import Dropdown from '@/Components/Dropdown.vue';
-// import DropdownLink from '@/Components/DropdownLink.vue';
-// import NavLink from '@/Components/NavLink.vue';
-// import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-// import { Link } from '@inertiajs/vue3';
-
-// const showingNavigationDropdown = ref(false);
 import Nav from '@/Components/Nav.vue';
 </script>
 
