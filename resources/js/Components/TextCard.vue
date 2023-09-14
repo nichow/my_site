314cc128316c3028aa08c87071a71dd52e1f7e39
Nic Howe <nichowe.garcia@gmail.com>
2023-09-14 15:06:34 -0400

    Make home page dynamic

--- resources/js/Components/TextCard.vue
+++ resources/js/Components/TextCard.vue
@@ -1,17 +1,31 @@
-<script setup>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
 const props = defineProps({
-    headerText: {
-        type: String
-    },
-    bodyText: {
-        type: String,
-    },
+    cardId: Number,
+    headerText: String,
+    bodyText: String,
 });
+
+const isActive = ref(false);
+
+onMounted(() => {
+    isActive.value = props.cardId == 0 ? true: false;
+});
+
+defineExpose({
+    isActive
+})
+
 </script>
 
 <template>
-    <div class="text-card flex flex-col">
+    <div
+        class="text-card flex flex-col"
+        :class="{active : isActive}">
         <h2 class="header-text text-gray-900 dark:text-white">{{ headerText }}</h2>
-        <div class="body-text text-gray-900 dark:text-white"><span style="white-space: pre-line;">{{ bodyText }}</span></div>
+        <div class="body-text text-gray-900 dark:text-white">
+            <span style="white-space: pre-line;">{{ bodyText }}</span>
+        </div>
     </div>
 </template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
-<script setup>
-import { Head, Link } from '@inertiajs/vue3';
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+import { Head } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
 import TextCard from '@/Components/TextCard.vue';
 
@@ -13,34 +14,68 @@ defineProps({
         required: true,
     },
 });
+
+type Card = {
+    id: number,
+    headerText: string,
+    bodyText: string,
+    isActive: boolean
+}
+
+const cards = ref([
+    { id: 0, headerText: 'Welcome to my Site!', bodyText: `My name is Nicolas Howe Garcia, and you've found my website! I am a software engineer with more hobbies than time. I use this site to showcase and document my personal projects; and also to host my blog, media reviews, and other various writings.
+    
+                    As of 2022 I've been employed by Nexxen (formerly Tremor Video) as a Technical Solutions Engineer for their demand side platform. I am not currently seeking new employment, but inquiries are always welcome. My resume is hosted here as well if needed.
+    
+                    I love media and writing about it! I'll review anything I feel the need to and host it all on this site, and I'll surely wax poetic about the subject on my blog. Go check out the archives if that interests you!
+    
+                    I do my best to update consistently, but sometimes life gets in the way.` },
+    { id: 1, headerText: 'Summary of the Site', bodyText: `As for this site, I built it with Laravel, using Vue.js for the front-end and Postgres for the database. I picked Laravel mostly to experiment, but I wanted something implementing a more traditional MVC pattern that also had the power to render nice looking SPAs like this. 
+                    
+                    The Lorem ipsum text is derived from sections 1.10.32 and 1.10.33 of Cicero's De finibus bonorum et malorum.[7][8] The physical source may have been the 1914 Loeb Classical Library edition of De finibus, where the Latin text, presented on the left-hand (even) pages, breaks off on page 34 with
+    
+                    The discovery of the text's origin is attributed to Richard McClintock, a Latin scholar at Hampden-Sydney College. McClintock connected Lorem ipsum to Cicero's writing sometime before 1982 while searching for instances of the Latin word consectetur, which was rarely used in classical literature.[2] McClintock first published his discovery in a 1994 letter to the editor of Before & After magazine,[9] contesting the editor's earlier claim that Lorem ipsum held no meaning.[2]
+    
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.` }
+]);
+const cardsRef = ref([]);
+let current : number = 0;
+
+function sideNavClick(id: number) {
+    (cardsRef.value[current] as Card).isActive = false;
+    (cardsRef.value[id] as Card).isActive = true;
+
+    current = id;
+}
+
+onMounted(() => {
+    console.log(typeof cardsRef.value[0])
+});
+
 </script>
 
 <template>
     <Head title="Nicolas Howe" />
     <AuthenticatedLayout>
         <div
-        class="relative flex flex-col items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+        @change-text="(id: number) => console.log(id)"
+        class="relative flex min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
         >
+            <div class="side-nav flex flex-col p-6">
+                <button 
+                class="dark:text-white bg-black mb-1"
+                v-for="card in cards"
+                @click="sideNavClick(card.id)">
+                    {{ card.headerText }}
+                </button>
+            </div>
             <TextCard
-                headerText="Welcome to my Site!"
-                bodyText="My name is Nicolas Howe Garcia, and you've found my website! I am a software engineer with more hobbies than time. I use this site to showcase and document my personal projects; and also to host my blog, media reviews, and other various writings.
-                    
-                As of 2022 I've been employed by Nexxen (formerly Tremor Video) as a Technical Solutions Engineer for their demand side platform. I am not currently seeking new employment, but inquiries are always welcome. My resume is hosted here as well if needed.
-
-                I love media and writing about it! I'll review anything I feel the need to and host it all on this site, and I'll surely wax poetic about the subject on my blog. Go check out the archives if that interests you!
-
-                I do my best to update consistently, but sometimes life gets in the way."    
-            ></TextCard>
-            <TextCard
-                headerText="Summary of the Site"
-                bodyText="As for this site, I built it with Laravel, using Vue.js for the front-end and Postgres for the database. I picked Laravel mostly to experiment, but I wanted something implementing a more traditional MVC pattern that also had the power to render nice looking SPAs like this. 
-                    
-                The Lorem ipsum text is derived from sections 1.10.32 and 1.10.33 of Cicero's De finibus bonorum et malorum.[7][8] The physical source may have been the 1914 Loeb Classical Library edition of De finibus, where the Latin text, presented on the left-hand (even) pages, breaks off on page 34 with
-
-                The discovery of the text's origin is attributed to Richard McClintock, a Latin scholar at Hampden-Sydney College. McClintock connected Lorem ipsum to Cicero's writing sometime before 1982 while searching for instances of the Latin word consectetur, which was rarely used in classical literature.[2] McClintock first published his discovery in a 1994 letter to the editor of Before & After magazine,[9] contesting the editor's earlier claim that Lorem ipsum held no meaning.[2]
-
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."    
-        ></TextCard>
+            v-for="card in cards" ref="cardsRef"
+            :key="card.id"
+            :cardId="card.id"
+            :headerText="card.headerText"
+            :bodyText="card.bodyText"
+            />
     </div>
     </AuthenticatedLayout>
 </template>
